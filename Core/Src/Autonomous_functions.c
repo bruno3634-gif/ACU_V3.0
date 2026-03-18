0ac0cb25bf91f944319424b7fffc10f372023070
bruno3634-gif <82599521+bruno3634-gif@users.noreply.github.com>
2026-03-18 23:27:35 +0000

    prototype ASSI

--- Core/Src/Autonomous_functions.c
+++ Core/Src/Autonomous_functions.c
@@ -50,7 +50,47 @@ int ASSI_controll(uint8_t gpio_state, uint8_t ASSI_state){
 	/*
 	 * bit 0 -> Yellow
 	 * bit 1 -> Blue
+	 *
+	 * -------------------------------------------------------------------------
+	 * AS Status Indicator (ASSI) - Operational States (Ref: T 14.8)
+	 * -------------------------------------------------------------------------
+	 * AS STATE        | ILLUMINATION STATUS
+	 * ----------------|--------------------------------------------------------
+	 * AS Off          | Off						0
+	 * AS Ready        | Yellow (Continuous)		1
+	 * AS Driving      | Yellow (Flashing)			2
+	 * AS Emergency    | Blue (Flashing)			3
+	 * AS Finished     | Blue (Continuous)			4
+	 * -------------------------------------------------------------------------
+	 *During “AS Driving” and “AS Emergency” the ASSIs must be flashing continuously with a
+	 *frequency between 2 Hz and 5 Hz and a duty cycle of 50 %.
+	 *
 	 */
+
+	static unsigned long prev_time;
+
+	switch (0) {
+		case value:
+
+			break;
+		case(1):
+
+		break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		default:
+			break;
+	}
+
+
+
 }
 
 
