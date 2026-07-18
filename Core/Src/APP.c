9f62f5f77d5badaf1281c4f98364cc41a8dc0490
bruno3634-gif <82599521+bruno3634-gif@users.noreply.github.com>
2026-07-18 03:45:48 +0100

    switched hydraulic pressure conectors physicly

--- Core/Src/APP.c
+++ Core/Src/APP.c
@@ -192,7 +192,7 @@ void dbc_decode(){
 	case AUTONOMOUS_T26_AQT7_FRAME_ID:
 		struct autonomous_t26_aqt7_t rear_dynamics;
 		autonomous_t26_aqt7_unpack(&rear_dynamics, can_rx_data.tx_data, AUTONOMOUS_T26_AQT7_LENGTH);
-		//t24.Rear_Pressure.Hydraulic = autonomous_t26_aqt7_rear_brk_press_decode(rear_dynamics.rear_brk_press);
+		t24.Rear_Pressure.Hydraulic = autonomous_t26_aqt7_rear_brk_press_decode(rear_dynamics.rear_brk_press);
 		t24.REAR_PRESSURE_LAST_TX = can_rx_ringbuffer.queue[can_rx_ringbuffer.tail].arrival_time;
 		break;
 	case AUTONOMOUS_T26_AQT1_FRAME_ID:
@@ -201,9 +201,11 @@ void dbc_decode(){
 #if BYPASS_REAR_HYD_PRESSURE
 			// TEST BYPASS: ignore the CAN reading, synthesize a value consistent with
 			// rear_solenoid (which physically locks/releases the front line) instead.
-			t24.Rear_Pressure.Hydraulic = t24.rear_solenoid
+			/*t24.Rear_Pressure.Hydraulic = t24.rear_solenoid
 					? BYPASS_FRONT_HYD_PRESSURE_UNLOADED
-					: BYPASS_FRONT_HYD_PRESSURE_LOADED;
+					: BYPASS_FRONT_HYD_PRESSURE_LOADED;*/
+
+			//t24.Front_Pressure.Hydraulic = autonomous_t26_aqt1_frt_brk_press_decode(front_dynamics.frt_brk_press);
 #else
 			t24.Rear_Pressure.Hydraulic = autonomous_t26_aqt7_rear_brk_press_decode(rear_dynamics.rear_brk_press);
 #endif
